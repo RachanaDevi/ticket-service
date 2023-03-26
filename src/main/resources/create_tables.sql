c6010315d9520d37e47e5fc5a1e664051699f9e9
Rachana Devi <rachanadevi18@gmail.com>
2023-03-26 19:24:17 +0530

    Update database to be ticket instead of postgres and remove foreign key constraints as the keys are present in another database

--- src/main/resources/create_tables.sql
+++ src/main/resources/create_tables.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE ticket;
 CREATE TABLE tickets
 (
     id          serial PRIMARY KEY,
@@ -6,8 +7,8 @@ CREATE TABLE tickets
     status      VARCHAR(255),
     scheduled_timestamp   timestamp,
     creation_timestamp   timestamp,
-    place       VARCHAR(100),
-    FOREIGN KEY (customer_id) references customers (id)
+    place       VARCHAR(100)
+--     FOREIGN KEY (customer_id) references customers (id)
 );
 
 CREATE TABLE tickets_assigned
@@ -15,7 +16,7 @@ CREATE TABLE tickets_assigned
     id          serial PRIMARY KEY,
     ticket_id   serial,
     consultant_id serial,
-    FOREIGN KEY (consultant_id) references consultants (id),
+--     FOREIGN KEY (consultant_id) references consultants (id),
     FOREIGN KEY (ticket_id) references tickets (id)
 );
 
